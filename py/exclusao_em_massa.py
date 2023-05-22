1b906bdb7f772a97a8d037b8cf1eebbd27d0d2a3
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-05-22 11:47:26 -0300

    .

---
+++ py/exclusao_em_massa.py
@@ -0,0 +1,14 @@
+import pyautogui as pg
+from time import sleep as sl
+
+
+class GPSvista9():999
+def excluir():
+    sl(1.5)
+    pg.click(1084, 358)
+    pg.press('tab')
+    pg.press('enter')
+
+while True:
+    excluir()
+    sl(5)
